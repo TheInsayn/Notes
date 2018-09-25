f35ca6146adfe3dee7383c359cf3b178071cc9af
TheInsayn <mat.woess@gmail.com>
2018-09-25 15:54:43 +0200

    dependencies updated

--- app/src/main/java/com/mathias/android/notes/RecyclerAdapter.kt
+++ app/src/main/java/com/mathias/android/notes/RecyclerAdapter.kt
@@ -6,9 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-/**
- * Created by Mathias.
- */
 class RecyclerAdapter(private val mNoteList: List<Note>) : RecyclerView.Adapter<RecyclerAdapter.NoteViewHolder>() {
 
     inner class NoteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
